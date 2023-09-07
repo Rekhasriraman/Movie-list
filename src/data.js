77cc8f51b4d2ca7347d1ad6ddf641c1054b1a955
Rekhasriraman <rekha.sri85@gmail.com>
2023-09-07 12:39:23 +0200

    update

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 const data = {
-  //  baseUrl: 'http://localhost:5000/movies'
-   baseUrl: 'https://my-json-server.typicode.com/Rekhasriraman/movies-list/movies'
+    baseUrl: 'http://localhost:5000/movies'
+   //baseUrl: 'https://my-json-server.typicode.com/Rekhasriraman/movies-list/movies'
 };
 
 export default data;
